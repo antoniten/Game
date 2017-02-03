6089a0f99734f359ea02a394981d84a377f4a224
antoniten <ksusha123195@gmail.com>
2017-02-03 20:35:47 +0300

    добавление класса Player

--- app/src/main/java/com/example/student3/thegame/GameView.java
+++ app/src/main/java/com/example/student3/thegame/GameView.java
@@ -4,15 +4,19 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
 
+import static com.example.student3.thegame.R.drawable.player;
+
 /**
  * Created by student3 on 28.01.17.
  */
 public class GameView extends SurfaceView implements Runnable {
+    private Player player;
 
     private boolean playing;
     private Thread gameThread = null;
@@ -30,8 +34,20 @@ public class GameView extends SurfaceView implements Runnable {
         surfaceHolder = getHolder();
         paint = new Paint();
         friend = new Friend(context, screenX, screenY);
+        player = new Player(context, screenX, screenY);
     }
 
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                player.stopBoosting();
+                break;
+            case MotionEvent.ACTION_DOWN:
+                player.setBoosting();
+                break;
+        }
+        return true;
+    }
 
     @Override
     public void run(){
@@ -45,7 +61,10 @@ public class GameView extends SurfaceView implements Runnable {
             s.update(1);
         }
         friend.update(1);
+        player.update();
     }
+
+
 
     private void draw(){
         if(surfaceHolder.getSurface().isValid()){
@@ -61,6 +80,8 @@ public class GameView extends SurfaceView implements Runnable {
             }
 
             canvas.drawBitmap(friend.getBitmap(),friend.getX(),friend.getY(),paint);
+            canvas.drawBitmap(player.getBitmap(), player.getX(), player.getY(), paint);
+
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
     }
